dc3541998554aa81cb0a9805747a8cce7811ef4a
Josh Gwinn <jdgwinn911@gmail.com>
2018-07-02 15:06:10 -0400

    first fix

--- tdd_message.rb
+++ tdd_message.rb
@@ -3,6 +3,6 @@ require_relative "message.rb"
 
 class CeasarsMessage < Minitest::Test
     def test_that_hash_has_value
-        assert_equal({}, hash_encrypt(message()))
+        assert_equal([*0..25], hash_encrypt(message()))
     end
 end
