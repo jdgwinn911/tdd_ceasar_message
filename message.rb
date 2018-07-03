3d01908d537025b717283efb6d5a861a638895d6
Josh Gwinn <jdgwinn911@gmail.com>
2018-07-03 09:50:42 -0400

    on the right track now

--- message.rb
+++ message.rb
@@ -1,5 +1,20 @@
-def hash_encrypt(message)
-    letters = ("a".."z").to_a
-    letters = letters.map.with_index { |x, i| [i, x]}.to_h
-    p letters.keys
+
+def alpha(message) # message is input from test
+    newarr = [*"a".."z"]  # newarr is an array that contains a-z
+    var = message.split("") # splits the message into an array
+    newvar = "" # newvar is a variable that equals an empty string 
+    var.each_with_index do |val, index| # takes the message (input from test) that was split into an array and runs through it's index
+        if ("a".."z").include? (val.downcase)
+            index = newarr.index(val) # 
+            1.times{val = newarr[index + 5]} # looks through 1 time and jumps 5 places in index 
+            newvar += val # adds value to the variable newvar 
+
+        # elsif ("a".."z").include? (i.uppercase)
+        #     5.times{i = i.next}
+        #     message += i[-1]
+        #     return message
+         end
+    end
+    newvar
 end
+
--- tdd_message.rb
+++ tdd_message.rb
@@ -2,7 +2,12 @@ require "minitest/autorun"
 require_relative "message.rb"
 
 class CeasarsMessage < Minitest::Test
-    def test_that_hash_has_value
-        assert_equal([*0..25], hash_encrypt(message()))
+    def test_that_return_string
+        assert_equal("mjqqt", alpha("hello"))
     end
+
+    # def test_that_stuff_returns_right
+    #     assert_equal("mjqqt btwqi", alpha("hello world"))
+    # end
+
 end
