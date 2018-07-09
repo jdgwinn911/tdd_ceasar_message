bab17baeee9be1108da4d1f3d25a177518884937
Josh Gwinn <jdgwinn911@gmail.com>
2018-07-09 11:24:23 -0400

    decrypter fail(wrong message)

--- tdd_message.rb
+++ tdd_message.rb
@@ -23,9 +23,15 @@ class CeasarsMessage < Minitest::Test
         assert_equal("mjqqt btwqi42", alpha("hello world42"))
         assert_equal("MjQqT BtWqi42", alpha("HeLlO WoRld42"))
         assert_equal("MjQqt ! BtWqi", alpha("HeLlo ! WoRld"))
+        assert_equal("MJQQT * btwqi", alpha("HELLO * world"))
+        assert_equal("MjQqt17 !! BtWqi42", alpha("HeLlo17 !! WoRld42"))
     end
 
     def test_2that_upcase_spaces_and_special_chars_are_incorperated
-        assert_equal("HElLo WOrLd42!", alpha2("MJqQt BTwQi42!"))
+        assert_equal("czggj rjmgy42", alpha2("hello world42"))
+        assert_equal("CzGgJ RjMgy42", alpha2("HeLlO WoRld42"))
+        assert_equal("CzGgj ! RjMgy", alpha2("HeLlo ! WoRld"))
+        assert_equal("CZGGJ * rjmgy", alpha2("HELLO * world"))
+        assert_equal("MjQqt17 !! BtWqi42", alpha2("HeLlo17 !! WoRld42"))
     end
 end
