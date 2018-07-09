d710a78a2e469037b318ba1adc3f80510a0f37d9
Josh Gwinn <jdgwinn911@gmail.com>
2018-07-09 13:15:07 -0400

    removed the (puts and comments)

--- message.rb
+++ message.rb
@@ -1,23 +1,19 @@
 
 def alpha(message) # message is input from test
-    newarr = [*"a".."z"]  # newarr is an array that contains a-z
+    newarr = [*"a".."z"]  
     newarr2 = [*"A".."Z"]
-    var = message.split("") # splits the message into an array
-    newvar = "" # newvar is a variable that equals an empty string 
-    var.each_with_index do |val, index| # takes the message (input from test) that was split into an array and runs through it's index
+    var = message.split("") 
+    newvar = ""  
+    var.each_with_index do |val, index| 
         if ("a".."z").include? (val)
-            index = newarr.index(val) # 
+            index = newarr.index(val)  
             newarr.rotate!(5)
             val = newarr[index]
-            newvar += val # adds value to the variable newvar
+            newvar += val 
         elsif ("A".."Z").include? (val)
             index = newarr2.index(val) 
-            p val
-            p index
             newarr2.rotate!(5)
             val = newarr2[index]
-            p val
-            p index
             newvar += val 
         else 
             newvar += val
@@ -40,12 +36,8 @@ def alpha2(message)
             newvar += val 
         elsif ("A".."Z").include? (val)
             index = newarr2.index(val) 
-            p val
-            p index
             newarr2.rotate!(-5)
             val = newarr2[index]
-            p val
-            p index
             newvar += val 
         else 
             newvar += val
